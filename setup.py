b078c6996fbbc5d8295f29e573fe54f5af102c41
Kyle Gabriel <kizniche@users.noreply.github.com>
2018-10-16 14:22:58 -0400

    py3 changes

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from distutils.core import setup
 
-setup(name='PiPyADC',
+setup(name='PiPyADC-py3',
       version='1.0',
       description='PiPyADC for python3',
-      url='https://github.com/kizniche/PiPyADC',
-      py_modules=['pipyadc'],
+      url='https://github.com/kizniche/PiPyADC-py3',
+      py_modules=['pipyadc-py3'],
      )
