73b110bed74b8a510242aca4dbcbc4f67fdb33b8
Kyle Gabriel <kizniche@users.noreply.github.com>
2018-10-16 14:26:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='PiPyADC-py3',
       version='1.0',
       description='PiPyADC for python3',
       url='https://github.com/kizniche/PiPyADC-py3',
-      py_modules=['pipyadc-py3'],
+      py_modules=['pipyadc_py3'],
      )
