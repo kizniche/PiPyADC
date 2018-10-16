32e3c8fc7c81923ca6bb10b2b62bab2ebc803a41
Kyle Gabriel <kizniche@users.noreply.github.com>
2018-10-16 03:34:31 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Adafruit_BME280',
+      version='1.0',
+      description='PiPyADC for python3',
+      url='https://github.com/kizniche/PiPyADC',
+      py_modules=['pipyadc'],
+     )
